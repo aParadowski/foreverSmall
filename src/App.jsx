7d6403a5df2861f4f5968489583552ee882b090c
AnthonyParadowski <ajparadowski@gmail.com>
2019-07-08 20:56:37 -0600

    Updated linting and webpack config for jsx files

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+  withStyles, Card, AppBar, Toolbar, Typography, Button, IconButton,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import Logo from './logo.svg';
+import './App.css';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+    height: '100%',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  loginButton: {
+    marginLeft: 'auto',
+  },
+};
+
+function App(props) {
+  const { classes } = props;
+  return (
+    <>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit">
+              Welcome to Forever Small. Get them gainz.
+            </Typography>
+            <Button color="inherit" className={classes.loginButton}>Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <Card>
+        <Logo />
+      </Card>
+    </>
+  );
+}
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     const { classes } = this.props;
+
+//     return (
+//       <>
+//       <div className={classes.root}>
+//         <AppBar position="static">
+//           <Toolbar>
+//             <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+//               <MenuIcon />
+//             </IconButton>
+//             <Typography variant="h6" color="inherit">
+//               Welcome to Forever Small. Get them gainz.
+//             </Typography>
+//             <Button color="inherit" className={classes.loginButton}>Login</Button>
+//           </Toolbar>
+//         </AppBar>
+//       </div>
+//       <Card>
+//         <Logo />
+//       </Card>
+//       </>
+//     );
+//   }
+// }
+
+export default withStyles(styles)(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,58 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
       {
         test: /\.css$/,
-        loader: 'style-loader'
+        loader: 'style-loader',
       },
       {
         test: /\.css$/,
         loader: 'css-loader',
         query: {
           modules: true,
-          localIdentName: '[name]__[local]___[hash:base64:5]'
-        }
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+        },
       },
       {
         test: /\.svg$/,
         use: [
           {
-            loader: "babel-loader"
+            loader: 'babel-loader',
           },
           {
-            loader: "react-svg-loader",
+            loader: 'react-svg-loader',
             options: {
-              jsx: true
-            }
-          }
-        ]
+              jsx: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
